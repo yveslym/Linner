e96a8d78a14f3c2aad229a95228369ef852e184b
Yves Songolo <yveslym@Yvess-MacBook-Pro.local>
2018-08-22 15:05:44 -0700

    create line model and service

---
+++ Linner/View Controllers/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  Linner
+//
+//  Created by Yves Songolo on 8/21/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class LoginViewController: UIViewController {
+    // sign in button
+    @IBOutlet weak var googleButton: GIDSignInButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
+// authenticate with google
+extension LoginViewController: GIDSignInUIDelegate{
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        // ...
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        // ...
+    
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            // User is signed in
+            // ...
+        }
+    }
+}
