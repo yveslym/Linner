6d608afd975dee78072c511927cbc6c0b6583ab0
Yves Songolo <yveslym@Yvess-MacBook-Pro.local>
2018-08-26 17:34:49 -0700

    design the single post view

--- Linner/View Controllers/TaskDetailViewController.swift
+++ Linner/View Controllers/TaskDetailViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MapKit
+import Kingfisher
 
 class TaskDetailViewController: UIViewController {
 
     var post: Post!
+    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var postTitle: UILabel!
+    @IBOutlet weak var posterName: UILabel!
+    @IBOutlet weak var taskDuration: UILabel!
+    @IBOutlet weak var taskDescription: UILabel!
+    @IBOutlet weak var taskMap: MKMapView!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func returnButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        postTitle.text = post.title
+        posterName.text = post.clientName
+        taskDescription.text = post.description
+        self.imageVIew.kf.setImage(with: URL(string: post.posterImage)!)
+        addressToCoordinatesConverter(address: post.location)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,14 +48,20 @@ class TaskDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addressToCoordinatesConverter(address: String) {
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(address, completionHandler: { (placemarks, error) -> Void in
+            if error == nil {
+                if placemarks!.count != 0 {
+                    let annotation = MKPointAnnotation()
+                   
+                    annotation.coordinate = (placemarks?.first?.location?.coordinate)!
+                   // self.taskMap.showAnnotations([annotation], animated: true)
+                    self.taskMap.addAnnotation(annotation)
+//                    self.taskMap.selectedAnnotations(annotation, animated: true)
+                    self.taskMap.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
     }
-    */
-
 }
